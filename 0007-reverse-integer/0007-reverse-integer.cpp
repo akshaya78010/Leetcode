1ee86a2a1494640b46a6fa7805cc8c6434e4b70a
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2025-06-21 00:59:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        cout<<x<<'\n';
+        long long int r;
+        long long int res = 0;
+        while(x)
+        {
+           r = x%10;
+           res = res * 10+r;
+           x = x/10;
+        }
+        cout<<res<<'\n';
+        if(res > 2147483647 || res < -2147483647)
+        {
+            return 0;
+        }
+        return res;
+    }
+};
