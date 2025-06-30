90680153f06e269ca85064a676645aaa164a32b2
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2025-06-30 21:36:12 +0530

    Time: 86 ms (5.09%) | Memory: 52.8 MB (5.69%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+
+     map<int,int>mpp;
+    int index;
+    int length = 0;
+     for(int i =0; i < nums.size() ; i++)
+     {
+        if(mpp[nums[i]] == 0){
+        mpp[nums[i]] = i+1;
+        // cout<<nums[i]<<":"<<i+1<<'\n';
+        }
+     }
+
+     for(int i = nums.size() - 1; i >= 0 ; i--)
+     {
+        if(mpp[nums[i] - 1] > 0)
+        {
+            index = mpp[nums[i] - 1] - 1;
+            length = max(i - index + 1,length);
+          
+        }
+     }
+        return length;
+    }
+};
