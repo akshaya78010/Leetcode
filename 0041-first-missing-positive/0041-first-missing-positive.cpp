548797ad155bc10d4e53aee1dbf5f96c61218caa
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2026-07-21 13:38:06 +0530

    Time: 0 ms (100%), Space: 55.2 MB (20.03%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& arr) {
+    int firstMissingPositive(vector<int>& nums) {
         
-        int n = arr.size();
-
-        for(int i =0; i < n ; i++){
-            while(arr[i] >= 1 && arr[i] <= n && arr[i] != arr[arr[i] - 1]){
-                swap(arr[i],arr[arr[i] - 1]);
+         int no_of_zero = 0;
+        for(int i =0 ; i < nums.size() ; i++){
+            if(nums[i] <= 0){
+                nums[i] = 0;
+                no_of_zero += 1;
             }
         }
 
-        int res = -1;
+       int n = nums.size();
 
-        for(int i =0; i < n ; i++){
-            if(arr[i] != i+1){
-                res = i+1;
-                break;
+        int val;
+        for(int i = 1; i <= n ; i++){
+            val= abs(nums[i-1]);
+            if(val >= 1 && val <= n){
+                if(nums[val - 1] == 0){
+                    nums[val - 1] = -(n+1);
+                }
+                else if(nums[val-1] > 0){
+                    nums[val - 1] *= -1;
+                }
             }
         }
 
-        if(res == -1){
-            res = n+1;
-        }
+        // long long int original_sum = n*(n+1)/2;
+        // int current_sum = 0;
 
-        return res;
+       for(int i = 1 ; i <= n ; i++){
+        if(nums[i-1] >= 0){
+            return i;
+        }
+       }
+
+        return n+1;
     }
 };
