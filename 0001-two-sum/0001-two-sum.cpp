662c871a44b29aa71a10f7a3c37371925a1cae5f
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2026-07-22 13:51:54 +0530

    Time: 8 ms (38.34%), Space: 15.9 MB (7.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,22 +2,42 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
+
+        int n = nums.size();
+        //put all nums in map
+        for(int i = 0; i < nums.size() ; i++){
+            mpp[nums[i]] += 1;
+        }
+
+        int diff = 0;
         vector<int>res;
+        // bool flag = false;
+        int to_find = 0;
 
-        for(int i =0; i < nums.size() ; i++){
-            mpp[nums[i]] = i;
-        }
+        //find the other num
+        for(int i = 0; i < nums.size() ; i++){
+            diff = target - nums[i];
 
-        for(int i =0; i < nums.size() ; i++){
-            int diff = target - nums[i];
-            if(mpp.count(diff) && mpp[diff] != i){
-                res.push_back(i);
-                res.push_back(mpp[diff]);
-                sort(res.begin(),res.end());
-                break;
+            if(mpp.find(diff) != mpp.end()){
+                if(nums[i] == diff && mpp[nums[i]] <= 1){ continue;}
+               to_find = diff;
+               cout<<to_find;
+               res.push_back(i);
+               break;
+                }
             }
+
+    //find the index of other number
+    for(int i =0 ; i < n ; i++){
+        if(nums[i] == to_find && i != res[0]){
+            res.push_back(i);
+            break;
         }
+    }    
+
+    sort(res.begin(),res.end());
+
     return res;
     }
 };
