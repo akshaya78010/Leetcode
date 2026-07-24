534a74dc7c3161b0dd03430baa1967c938175af4
akshaya78010 <147473029+akshaya78010@users.noreply.github.com>
2026-07-24 22:00:59 +0530

    Time: 3 ms (37.72%), Space: 40.1 MB (81.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int pro = 1;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                pro = pro * nums[i];
+
+     int county =0;
+     int n = nums.size();
+
+     for(int i =0; i < n ; i++){
+        if(nums[i] == 0){
+            county += 1;
+        }
+     }
+
+     if(county == 0){
+        int total = 1;
+
+        for(int i =0 ; i < n ; i++){
+            total *= nums[i];
+        }
+
+        for(int i =0; i < n ; i++){
+            nums[i] = total/nums[i];
+        }
+     }
+     else if(county == 1){
+        int total = 1;
+
+        for(int i =0 ; i < n ; i++){
+            if(nums[i] != 0){
+            total *= nums[i];
             }
         }
-        int county = count(nums.begin(), nums.end(), 0);
-        if (county == 0) {
-            for (int i = 0; i < n; i++) {
-                nums[i] = pro / nums[i];
+
+        for(int i =0; i < n ; i++){
+            if(nums[i] != 0){
+            nums[i] = 0;
             }
-        } else {
-            if (county == 1) {
-                for (int i = 0; i < n; i++) {
-                    if (nums[i] == 0) {
-                        nums[i] = pro;
-                    } else {
-                        nums[i] = 0;
-                    }
-                }
-            } else if (county > 1) {
-                for (int i = 0; i < n; i++) {
-                    nums[i] = 0;
-                }
+            else if(nums[i] == 0){
+                nums[i] = total;
             }
         }
-        return nums;
+     }
+     else if(county > 1){
+        for(int i = 0; i < n ; i++){
+            nums[i] = 0;
+        }
+     }
+
+     return nums;
     }
 };
